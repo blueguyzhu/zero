fd6fa9b0b0072e36d9e07ab7b452391ceaaec280
blueguyzhu <blueguyzhu@gmail.com>
2014-10-31 10:47:41 +0100

    first commit

---
+++ TempusZero/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TempusZero
+//
+//  Created by Wenlu Zhang on 31/10/14.
+//  Copyright (c) 2014 TEMPUS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TempusZero/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TempusZero
+//
+//  Created by Wenlu Zhang on 31/10/14.
+//  Copyright (c) 2014 TEMPUS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
